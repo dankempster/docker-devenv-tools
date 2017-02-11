bda36bb08e5bbb79e8816f4a02ebda0f140dae41
Dan Kempster <dev@dankempster.co.uk>
2017-02-11 10:03:23 +0000

    Add wrapper script to launch composer commands within docker containers.

--- bin/composer.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -u 82:82 -v $(pwd):/project -v composer_alpine:/composer -v composer_cache_alpine:/composer-cache -w /project dankempster/composer:alpine --ansi $*
